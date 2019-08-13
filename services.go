d84e98a8ea81738dbef38d31fa90bb5695f4dbcd
Alex Taran <a.taran@cti.ru>
2019-08-13 16:01:17 +0300

    dev

--- services.go
+++ services.go
@@ -1,51 +1,54 @@
 package zabbix
 
+import "encoding/json"
+
 // Service Zabbix object
 type Service struct {
-	ServiceID string
-	Algorithm uint8
-	Name      string
-	ShowSLA   uint8
-	SortOrder uint16
-	GoodSLA   float32
-	Status    uint8
-	TriggerID string
+	ServiceID    string              `json:"serviceid"`
+	Algorithm    uint8               `json:"algorithm"`
+	Name         string              `json:"name"`
+	ShowSLA      uint8               `json:"showsla"`
+	SortOrder    uint16              `json:"sortorder"`
+	GoodSLA      float32             `json:"goodsla"`
+	Status       uint8               `json:"status"`
+	TriggerID    string              `json:"triggerid"`
+	Dependencies []ServiceDependency `json:"dependencies"`
 }
 
 // ServiceTime Zabbix object
 type ServiceTime struct {
-	TimeID    string
-	ServiceID string
-	TSFrom    uint16
-	TSTo      uint16
-	Type      uint8
-	Note      string
+	TimeID    string `json:"timeid"`
+	ServiceID string `json:"serviceid"`
+	TSFrom    uint16 `json:"ts_from"`
+	TSTo      uint16 `json:"ts_to"`
+	Type      uint8  `json:"type"`
+	Note      string `json:"note"`
 }
 
 // ServiceDependency Zabbix object
 type ServiceDependency struct {
-	LinkID        string
-	ServiceDownID string
-	ServiceUpID   string
-	Soft          uint8
+	LinkID        string `json:"linkid"`
+	ServiceDownID string `json:"servicedownid"`
+	ServiceUpID   string `json:"serviceupid"`
+	Soft          uint8  `json:"soft"`
 }
 
 // ServiceAlarm Zabbix object
 type ServiceAlarm struct {
-	ServiceAlarmID string
-	ServiceID      string
-	Clock          uint32
-	Value          uint8
+	ServiceAlarmID string `json:"servicealarmid"`
+	ServiceID      string `json:"serviceid"`
+	Clock          uint32 `json:"clock"`
+	Value          uint8  `json:"value"`
 }
 
 // GetServices gets zabbix IT Services
-func (w *APIWrapper) GetServices(params map[string]interface{}) (services interface{}, err error) {
+func (w *APIWrapper) GetServices(params map[string]interface{}) (services []Service, err error) {
 	req := requestConstruct("service.get")
 	req.Params = params
 	res, err := req.Send(w)
 	if err != nil {
 		return
 	}
-	services = res
+	err = json.Unmarshal(res.Result.([]byte), &services)
 	return
 }
